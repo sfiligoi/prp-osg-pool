f2901b58d9bd78fe04574c38d51f3cb476c36be3
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-03-07 11:25:53 -0800

    Add preset-osg-wn-users

---
+++ k8s/add_image_users.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# List of all supported users
+groupadd -g 3001 osg && useradd -u 3001 -g 3001 -s /bin/bash osg
+groupadd -g 3002 icecube && useradd -u 3002 -g 3002 -s /bin/bash icecube
+groupadd -g 3003 glow && useradd -u 3003 -g 3003 -s /bin/bash glow
+
