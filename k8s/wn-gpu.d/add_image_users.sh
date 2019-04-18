e30aa0eaba86c19c54d3d5977ac0d3147885673b
Igor Sfiligoi <isfiligoi@ucsd.edu>
2019-04-18 09:29:22 -0700

    Update list of users

--- k8s/wn-gpu.d/add_image_users.sh
+++ k8s/wn-gpu.d/add_image_users.sh
@@ -4,4 +4,5 @@
 groupadd -g 3001 osg && useradd -u 3001 -g 3001 -s /bin/bash osg
 groupadd -g 3002 icecube && useradd -u 3002 -g 3002 -s /bin/bash icecube
 groupadd -g 3003 glow && useradd -u 3003 -g 3003 -s /bin/bash glow
+groupadd -g 3004 ligo && useradd -u 3004 -g 3004 -s /bin/bash ligo
 
