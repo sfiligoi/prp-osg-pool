35344418d14d6aa71558bc70d78e9a7cd60511c9
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-05-08 10:01:12 -0700

    Add fermigli user

--- k8s/wn-gpu.d/add_image_users.sh
+++ k8s/wn-gpu.d/add_image_users.sh
@@ -6,4 +6,5 @@ groupadd -g 3002 icecube && useradd -u 3002 -g 3002 -s /bin/bash icecube
 groupadd -g 3003 glow && useradd -u 3003 -g 3003 -s /bin/bash glow
 groupadd -g 3004 ligo && useradd -u 3004 -g 3004 -s /bin/bash ligo
 groupadd -g 3005 xenon.biggrid.nl && useradd -u 3005 -g 3005 -s /bin/bash xenon.biggrid.nl
+groupadd -g 3006 fermigli && useradd -u 3006 -g 3006 -s /bin/bash fermigli
 
