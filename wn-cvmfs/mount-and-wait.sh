fc2fc489548ac7d5a6f3f06cf074a4759fe8c0dc
Igor Sfiligoi <isfiligoi@ucsd.edu>
2020-05-10 09:21:14 -0700

    Add additional logging

--- wn-cvmfs/mount-and-wait.sh
+++ wn-cvmfs/mount-and-wait.sh
@@ -18,7 +18,7 @@ if [ "x${MOUNT_REPOS}" == "x" ]; then
 fi
 
 # do not die on signal, try to complete
-trap "echo Signal-Received" SIGTERM SIGINT
+trap "echo Signal-Received   | tee -a /cvmfs/cvmfs-pod.log" SIGTERM SIGINT
 
 mps=""
 for mp in `echo ${MOUNT_REPOS} |tr , ' '` ; do 
@@ -28,7 +28,7 @@ for mp in `echo ${MOUNT_REPOS} |tr , ' '` ; do
  if [ ${rc} -eq 0 ] ; then
    mps="$mp $mps" #save them in reverse order
  else
-   echo "Failed to mount $mp" 1>&2
+   echo "Failed to mount $mp"  | tee -a /cvmfs/cvmfs-pod.log
 
    # cleanup
    for mp1 in $mps; do
@@ -40,9 +40,9 @@ done
 
 echo "$mps" > /etc/mount-and-wait.mps
 
-echo "CVMFS mountpoints started: $mps"
+echo "CVMFS mountpoints started: $mps"  | tee -a /cvmfs/cvmfs-pod.log
 /usr/local/sbin/wait-only.sh
-echo "Terminating"
+echo "Terminating"   | tee -a /cvmfs/cvmfs-pod.log
 
 # cleanup
 
@@ -52,25 +52,25 @@ for mp1 in $mps; do
      umount /cvmfs/${mp1}
      rc=$?
      if [ $rc -ne 0 ]; then
-       echo "Failed unmounting ${mp1}"
+       echo "Failed unmounting ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
      else
        rmdir /cvmfs/${mp1}
-       echo "Unmounted ${mp1}"
+       echo "Unmounted ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
      fi
    fi
 done
 
 # now do a pass with the most fail-safe option possible
 for mp1 in $mps; do
-  echo "Attempting lazy umount of ${mp1}"
+  echo "Attempting lazy umount of ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
   umount -l /cvmfs/${mp1}
   if [ $? -eq 0 ]; then 
-   echo "Lazy unmounted ${mp1}"
+   echo "Lazy unmounted ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
   fi
 done
 
 # wait a tiny bit to make sure everything is cleaned up properly
 sleep 2
 
-echo "Bye"
+echo "Bye"  | tee -a /cvmfs/cvmfs-pod.log
 
--- wn-cvmfs/unmount-and-terminate.sh
+++ wn-cvmfs/unmount-and-terminate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # do not die on signal, try to complete
-trap "echo Signal-Received" SIGTERM SIGINT
+trap "echo Unmount-Signal-Received  | tee -a /cvmfs/cvmfs-pod.log" SIGTERM SIGINT
 
 maxtries=25
 # prevent startup race condition
 tries=0
 while [ ! -f /etc/mount-and-wait.pid ]; do
-     echo "mount-and-wait.pid"
+     echo "Missing mount-and-wait.pid"  | tee -a /cvmfs/cvmfs-pod.log
      sleep 1
      let tries=1+$tries
      if [ $tries -ge $maxtries ]; then
@@ -26,7 +26,7 @@ for mp1 in $mps; do
    rc=$?
    while [ $rc -ne 0 ]; do
      sleep 1
-     echo "Retrying unmounting ${mp1}"
+     echo "Retrying unmounting ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
      umount /cvmfs/${mp1}
      rc=$?
      let tries=1+$tries
@@ -35,7 +35,7 @@ for mp1 in $mps; do
      fi
    done
    rmdir /cvmfs/${mp1}
-   echo "Unmounted ${mp1}"
+   echo "Unmounted ${mp1}"  | tee -a /cvmfs/cvmfs-pod.log
 done
 
 
@@ -43,6 +43,7 @@ pidk=`cat /etc/mount-and-wait.pid`
 if [ "x${pidk}" != "x" ]; then
   # hard kill the waiting process, should not be doing anything but sleep
   kill -9 "$pidk"
+  echo "Killed $pidk"   | tee -a /cvmfs/cvmfs-pod.log
 else
-  echo "WARNING: /etc/mount-and-wait.pid not found"
+  echo "WARNING: /etc/mount-and-wait.pid not found"  | tee -a /cvmfs/cvmfs-pod.log
 fi
