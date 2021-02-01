8add930164c8296657fe455e62871000864f6a29
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-02-01 09:13:10 -0800

    Enhance the wrapper to alos account for -p, -C and --containall

--- wn/singularity_npid.sh
+++ wn/singularity_npid.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# simple singularity wrapper that doesn't allow the --pid option
+# simple singularity wrapper that doesn't allow the -p or --pid option
+# also rewrite -C to remove the -p option
 # Disclaimer: Based on 
 #  https://wiki-dev.bash-hackers.org/scripting/posparams
 #
@@ -13,8 +14,27 @@ do
     if ! ((eoo)); then
 	case "$1" in
 	  --pid)
+              # pretend was not passed
 	      shift
 	      ;;
+          -p)
+              # pretend was not passed
+              shift
+              ;;
+          --containall)
+              options+=("--contain")
+              options+=("--cleanenv")
+              options+=("--ipc")
+              # but not --pid
+              shift
+              ;;
+          -C)
+              options+=("-c")
+              options+=("-e")
+              options+=("-i")
+              # but not -p
+              shift
+              ;;
 	  --)
 	      eoo=1
 	      options+=("$1")
