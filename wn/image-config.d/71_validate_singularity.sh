371fe788f1f00ce31a34dcec4fb3d606a03b5e94
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-10-17 14:25:17 -0700

    Link to singularity_npid

--- wn/image-config.d/71_validate_singularity.sh
+++ wn/image-config.d/71_validate_singularity.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# condor will use the wrapper /usr/bin/singularity_npid.sh
-if [ -f "/usr/bin/singularity_npid.sh" ]; then
+if [ -f "/usr/bin/singularity" ]; then
   # only test for singularity functionality if singularity is present
   # may not be in all pods
 
-  /usr/bin/singularity_npid.sh exec --contain --ipc --pid --bind /cvmfs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el7:latest /usr/bin/dc -e "3 5 + p"
+  /usr/bin/singularity exec --contain --ipc --pid --bind /cvmfs /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el7:latest /usr/bin/dc -e "3 5 + p"
   rc=$?
 
   if [ $rc -ne 0 ]; then
