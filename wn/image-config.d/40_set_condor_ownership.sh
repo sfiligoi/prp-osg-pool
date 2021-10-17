f8bbb9c460acd913e515232a75a154a858f73bc1
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-10-17 12:24:25 -0700

    Use the provisionerr WN image

--- wn/image-config.d/40_set_condor_ownership.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chown condor:condor /var/lib/condor/execute
-chmod o-w /var/lib/condor/execute
-
-chown condor:condor /var/lib/condor/spool
-chmod o-w /var/lib/condor/spool
-
--- wn/image-config.d/60_add_users.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Condor needs the proper users in place
-source /usr/local/sbin/add_image_users.sh
