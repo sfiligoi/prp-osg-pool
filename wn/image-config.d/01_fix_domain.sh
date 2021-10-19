d563ba4da8cdb47559f9834255f9465e668cf5dc
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-10-19 13:14:11 -0700

    Add namespace to the domain

--- wn/image-config.d/01_fix_domain.sh
+++ wn/image-config.d/01_fix_domain.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ 'x${K8S_NAMESPACE}' == 'x' ]; then
+  K8S_NAMESPACE='prp'
+fi
+
 # k8s nodes have no domain, which is annoying
 # Add it here
-cp /etc/hosts /tmp/hosts && sed 's/\(osg-wn-.*\)/\1\.optiputer.net \1/' /tmp/hosts > /etc/hosts && rm -f /tmp/hosts
+cp /etc/hosts /tmp/hosts && sed "s/\(osg-wn-.*\)/\1\.${K8S_NAMESPACE}.optiputer.net \1/" /tmp/hosts > /etc/hosts && rm -f /tmp/hosts
