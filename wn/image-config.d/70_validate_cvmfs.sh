257aedadc1d5eb92540a8c1d01da25e8dc86b386
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-02-28 15:13:04 -0800

    Add scripts/monitor_cvmfs.sh

--- wn/image-config.d/70_validate_cvmfs.sh
+++ wn/image-config.d/70_validate_cvmfs.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
+if [ "x${CVMFS_REPOS}" == "x" ]; then
+  CVMFS_REPOS=`(cd /cvmfs && /usr/bin/ls -F -d * | awk 'BEGIN{l=""}/\//{split($0,a,"/"); if (l!="") l=l ","; l=l a[1]}END{print l}')`
+  echo "INFO: `date` Auto-detected CVMFS mounts: ${CVMFS_REPOS}"
+fi
+
+for d in `echo ${CVMFS_REPOS} |tr , ' '` ; do
+    if [ ! -d "/cvmfs/${d}" ]; then
+      echo "ERROR: `date` Not mounted: /cvmfs/${d}. "
+      exit 1
+    fi
+    if [ "`ls /cvmfs/${d} |wc -l`" -lt 2 ]; then
+      echo "ERROR: `date` Empty dir: /cvmfs/${d}. "
+      exit 1
+    fi
+done
+
 # oasis should be always present
 # pick a well defined file, e.g. singularity
 ls -l /cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity
 rc=$?
 
 if [ $rc -ne 0 ]; then
-  echo "Oasis CVMFS not found, retrying"
-  sleep 5
-  ls -l /cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity
-  rc=$?
-fi
-
-if [ $rc -ne 0 ]; then
-  echo "Oasis CVMFS not found, retrying"
-  sleep 15
-  ls -l /cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity
-  rc=$?
-fi
-
-if [ $rc -ne 0 ]; then
-  echo "Oasis CVMFS not found, retrying"
-  sleep 45
-  ls -l /cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity
-  rc=$?
-fi
-
-if [ $rc -ne 0 ]; then
-  echo "Oasis CVMFS not found!"
+  echo "ERROR: `date` Oasis CVMFS not readable (/cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity)"
   exit 1
 fi
 
-echo "# detected CVMFS mounts" > /etc/condor/config.d/10_cvmfs.conf
-for d in `(cd /cvmfs && /usr/bin/ls -d *)`; do
+
+echo "# configured CVMFS mounts" > /etc/condor/config.d/10_cvmfs.conf
+for d in `echo ${CVMFS_REPOS} |tr , ' '` ; do
   v=`echo $d | sed 's/\./_/g' | sed 's/-/_/g'`
   echo "HAS_CVMFS_$v=True" >> /etc/condor/config.d/10_cvmfs.conf
   echo 'STARTD_EXPRS = $(STARTD_EXPRS)' HAS_CVMFS_$v >> /etc/condor/config.d/10_cvmfs.conf
 done
 
+# keep monitoring the CVMFS mounts indefinitely
+/usr/sbin/monitor_cvmfs.sh "${CVMFS_REPOS}" </dev/null &
