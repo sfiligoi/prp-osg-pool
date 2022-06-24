082c3923eab266130cb8bbb5f4c6d069c515ac5e
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-06-24 08:52:29 -0700

    Add max_submit_pods_per_cluster and remove hardcoded values

--- provisioner/image/provisioner_main.py
+++ provisioner/image/provisioner_main.py
@@ -15,7 +15,7 @@ import prp_provisioner.provisioner_logging as provisioner_logging
 import prp_provisioner.provisioner_htcondor as provisioner_htcondor
 import prp_provisioner.event_loop as event_loop
 
-def main(log_fname, namespace, cvmfs_mounts, max_pods_per_cluster=20, sleep_time=60):
+def main(log_fname, namespace, cvmfs_mounts):
    fconfig = configparser.ConfigParser()
    fconfig.read(('pod.conf','osg_provisioner.conf'))
    kconfig = provisioner_k8s.ProvisionerK8SConfig(namespace)
@@ -26,10 +26,8 @@ def main(log_fname, namespace, cvmfs_mounts, max_pods_per_cluster=20, sleep_time
    kconfig.app_name = 'osg-wn'
    cconfig.app_name = 'osg-wn'
 
-   if 'k8s' in fconfig:
-      kconfig.parse(fconfig['k8s'])
-   else:
-      kconfig.parse(fconfig['DEFAULT'])
+   kfconfig = fconfig['k8s'] if ('k8s' in fconfig) else fconfig['DEFAULT']
+   kconfig.parse(kfconfig)
 
    hfconfig = fconfig['htcondor'] if ('htcondor' in fconfig) else fconfig['DEFAULT']
    cconfig.parse(hfconfig)
@@ -52,7 +50,11 @@ def main(log_fname, namespace, cvmfs_mounts, max_pods_per_cluster=20, sleep_time
    k8s_obj = provisioner_k8s.ProvisionerK8S(kconfig)
    k8s_obj.authenticate()
 
-   el = event_loop.ProvisionerEventLoop(log_obj, schedd_obj, collector_obj, k8s_obj, max_pods_per_cluster)
+   max_pods_per_cluster = int(lfconfig.get('max_pods_per_cluster','20'))
+   max_submit_pods_per_cluster = int(lfconfig.get('max_submit_pods_per_cluster','1000'))
+   sleep_time = int(fconfig['DEFAULT'].get('sleep_time','120'))
+
+   el = event_loop.ProvisionerEventLoop(log_obj, schedd_obj, collector_obj, k8s_obj, max_pods_per_cluster, max_submit_pods_per_cluster)
    while True:
       log_obj.log_debug("[Main] Iteration started, schedd whitelist='%s'"%schedd_whitelist)
       try:
